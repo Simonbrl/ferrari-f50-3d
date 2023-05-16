3c7af551e1fa7f82f4c7325576bcfb56305c8ca5
Flavian Magrit <flavian.magrit@decathlon.com>
2023-05-16 10:25:00 +0200

    feat(animating): rotating on scroll

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import { Canvas } from "@react-three/fiber";
 import Scene from "./components/Scene";
 
 const App = () => {
-
   return (
     <Canvas>
       <Scene />
     </Canvas>
-  )
-}
+  );
+};
 
 export default App;
